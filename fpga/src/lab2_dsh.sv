031ed4466eab38c105b9366375150b8fd8f6ad95
D. Sebastian Heredia <sebastianheredi4@gmail.com>
2025-09-07 00:11:35 -0700

    Update lab2_dsh.sv

--- fpga/src/lab2_dsh.sv
+++ fpga/src/lab2_dsh.sv
@@ -3,15 +3,18 @@
 // September 6, 2025
 // lab2_DSH.sv contains code to instantiate sevensegment.sv to light up a dual 7-segment display and display the sum of the two numbers
 // in binary on 5 LEDS. 
+`timescale 1ns/1ps
 
-module lab2_dsh (	input logic 	reset,
-					input logic		[3:0]s0,
-					input logic 	[3:0]s1,
-					output logic 	select,
-					output logic 	notselect, 
-					output logic 	[4:0]led,
-					output logic 	[6:0]seg
+module lab2_dsh (	input logic 			reset,
+					input logic		[3:0]	s0,
+					input logic 	[3:0]	s1,
+					output logic 			select,
+					output logic 			notselect, 
+					output logic 	[4:0]	led,
+					output logic 	[6:0]	seg
 				);
+	
+	logic [3:0]s;	// internal logic output from mux
 	
 // Idiomatc Adder (5-bits on LEDs)
 	assign led = s0 + s1;				// led[0] = P1, led[1] = P2, ... assign in hardware designer
@@ -26,7 +29,7 @@ module lab2_dsh (	input logic 	reset,
   
     // Counter for 2.4Hz
 	always_ff @(posedge int_osc) begin
-		if (reset == 0)begin
+		if (reset == 0)begin			// intentionall active LOW
 			counter   <= 0;
 			state <= 0;
 		end 
@@ -51,12 +54,11 @@ module lab2_dsh (	input logic 	reset,
 // Idiomatic mux taking in s0 and s1 and outputting 4'b into sevensegment
 	assign s =  state ? s0:s1;		// if state == 1, then mux choses s0 value, otherwise s1.
 
-	// Instantiate SevenSegment module
+	// Instantiate sevensegment module
     sevensegment sevensegment(s, seg);
 	
 endmodule 
 	
 	
-
 
 

2c8b1c5362a1f0755eabc6e787dc3b6cb705d727
D. Sebastian Heredia <sebastianheredi4@gmail.com>
2025-09-08 20:53:02 -0700

    Update lab2_dsh.sv

--- fpga/src/lab2_dsh.sv
+++ fpga/src/lab2_dsh.sv
@@ -1,7 +1,7 @@
 // E155: Lab 2 - Multiplexed 7-Segment Display
 // Sebastian Heredia, dheredia@g.hmc.edu
 // September 6, 2025
-// lab2_dsg.sv contains code to instantiate sevensegment.sv to light up a dual 7-segment display and display the sum of the two numbers
+// lab2_DSH.sv contains code to instantiate sevensegment.sv to light up a dual 7-segment display and display the sum of the two numbers
 // in binary on 5 LEDS. 
 `timescale 1ns/1ps
 
@@ -15,7 +15,7 @@ module lab2_dsh (	input logic				clk,
 					output logic 	[6:0]	seg
 				);
 	
-	logic [3:0]s;	// internal logic output from mux
+	logic [3:0]s;	// Internal logic output from mux
 	
 // Idiomatc Adder (5-bits on LEDs)
 	assign led = s0 + s1;				// led[0] = P1, led[1] = P2, ... assign in hardware designer
@@ -25,38 +25,35 @@ module lab2_dsh (	input logic				clk,
 	logic [31:0]counter = 0;			// Start with counter at 0
 	logic 		state = 0;
 	
-	HSOSC #(.CLKHF_DIV(2'b01)) 													// Divide clk to 24MHz
+	HSOSC #(.CLKHF_DIV(2'b01))   		// Divides clk from 48MHz to 24MHz
 		hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
   
-    // Counter for 2.4Hz
+    // Counter for 240Hz, a frequency that is much greater than the frequency the human eye can observe
 	always_ff @(posedge int_osc) begin
-		if (reset == 0)begin			// intentionall active LOW
+		if (reset == 0)begin			// Intentionally active LOW
 			counter   <= 0;
 			state <= 0;
 		end 
-		
-		// 24MHz / 2.4Hz = 10E6													// UPDATE
-		// 10E6 / 2 = 5E6 for toggle (ON-OFF)
-		// Subtract 1 since [4,999,999:0] is 5E6 bits
 												
-		else if (counter == 50000-1) begin
+		else if (counter == 50000-1) begin		// 50,000 counts since indexing from 0-49,999
 			counter   <= 0;
-			state <= ~state;  			// Toggle LED OFF after 5E6 counts
+			state <= ~state;  			// Toggle LED OFF after 5E4 counts
 		end 
 		
 		else begin
-			counter <= counter + 1;		// Continue incrementing counter
+			counter <= counter + 1;		// Continue incrementing counter for next toggle
 		end
 	end
 	
+	// Using 1-bit signal to toggle between left and right displays on the dual 7-segment display
 	assign select = state;
-	assign notselect = ~state;
+	assign notselect = ~state;			
 		
-// Idiomatic mux taking in s0 and s1 and outputting 4'b into sevensegment
-	assign s =  state ? s0:s1;		// if state == 1, then mux choses s0 value, otherwise s1.
+	// Idiomatic mux taking in s0 and s1 and outputting 4'b into sevensegment
+	assign s =  state ? s0:s1;		// ternary operator: if state == 1, then mux choses s0 value, otherwise s1.
 
 	// Instantiate sevensegment module
-    sevensegment sevensegment (s, seg);
+    sevensegment sevensegment(s, seg);
 	
 endmodule 
 	
